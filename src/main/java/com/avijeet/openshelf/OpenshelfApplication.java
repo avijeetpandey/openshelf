295bfd3b19665218e3a53b3746edaa332c514585
Avijeet Pandey <avijeetpandey25@gmail.com>
2026-04-07 04:26:17 +0530

    add: init project

---
+++ src/main/java/com/avijeet/openshelf/OpenshelfApplication.java
@@ -0,0 +1,13 @@
+package com.avijeet.openshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenshelfApplication.class, args);
+    }
+
+}
